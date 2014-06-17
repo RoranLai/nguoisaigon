480df8c22b24e012fc1c6c273e15d0b826738de2
NgheTC <mrnghe.it@gmail.com>
2014-06-18 00:34:04 +0700

    [Nghe-TC] Commit layout màn hình News

--- src/com/nguoisaigon/activity/NewsActivity.java
+++ src/com/nguoisaigon/activity/NewsActivity.java
@@ -1,15 +1,79 @@
 package com.nguoisaigon.activity;
 
-import com.nguoisaigon.R;
+import java.sql.Date;
+import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class NewsActivity extends Activity
-{
+import com.nguoisaigon.R;
+import com.nguoisaigon.entity.NewsInfo;
+import com.nguoisaigon.util.WebService.WebServiceDelegate;
+
+public class NewsActivity extends Activity implements WebServiceDelegate {
+	ArrayList<NewsInfo> listNews;
+
+	@SuppressLint("SimpleDateFormat")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_layout);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news_layout);
+		this.listNews = new ArrayList<NewsInfo>();
+		String jsonData = "[{'createDate':'2014-03-24T18:37:01',"
+				+ "'newsContent':'tin tức mới nhất',"
+				+ "'newsId':'e8f3ac76-1a23-4fbc-91fe-1a293a5b723b';"
+				+ "'ownerInfo':'abc';" + "'title':'nguoi sai gon';"
+				+ "},{ 'createDate':'2014-03-18T01:44:41';"
+				+ "'newsContent':'test test';"
+				+ "'newsId':'d854144f-49b8-4cd0-8732-b5123679e88d';"
+				+ "'ownerInfo':'nnn';" + "'title':'Tin moi';}]";
+		try {
+
+			JSONArray listNewsJSON = new JSONArray(jsonData);
+			for (int i = 0; i < listNewsJSON.length(); i++) {
+				JSONObject newsJSON = listNewsJSON.getJSONObject(i);
+				NewsInfo news = new NewsInfo();
+				news.setCreateDate((Date) newsJSON.get("createDate"));
+				news.setNewsContent(newsJSON.getString("newsContent"));
+				news.setNewsId(newsJSON.getString("newsId"));
+				news.setOwnerInfo(newsJSON.getString("ownerInfo"));
+				news.setTitle(newsJSON.getString("title"));
+				this.listNews.add(news);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+//		TextView tvCreateDate = (TextView) findViewById(R.id.tvCreateDate);
+//		TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+//		TextView tvContent = (TextView) findViewById(R.id.tvContent);
+//		NewsInfo news = this.listNews.get(0);
+//		SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
+//		String createDate = formater.format(news.getCreateDate());
+//		tvCreateDate.setText(createDate);
+//		tvTitle.setText(news.getTitle());
+//		tvContent.setText(news.getNewsContent());
+
+		// Download data
+		// WebService ws = new WebService(this);
+		// Calendar cal = Calendar.getInstance();
+		// cal.getTime();
+		// ws.setGettingNewsData(cal.getTime());
+		// ws.execute();
+
+	}
+
+	@Override
+	public void taskCompletionResult(JSONObject result) {
+		System.out.println(result.toString());
+	}
+
+	public void btnCloseClick() {
+		this.finish();
 	}
 }
--- src/com/nguoisaigon/util/WebService.java
+++ src/com/nguoisaigon/util/WebService.java
@@ -2,8 +2,8 @@ package com.nguoisaigon.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -18,83 +18,83 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.util.Log;
 
-public class WebService extends AsyncTask <String, Void, JSONObject>
-{
+public class WebService extends AsyncTask<String, Void, JSONObject> {
 	public interface WebServiceDelegate {
-	    public void taskCompletionResult(JSONObject result);
+		public void taskCompletionResult(JSONObject result);
 	}
-	
+
 	private String SERVER_URL = "http://rest.itsleek.vn";
 	private String url;
 	private WebServiceDelegate delegate;
-	
+
 	/**
 	 * Constructor with delegate
 	 */
-	public WebService(WebServiceDelegate vdelegate)
-	{
+	public WebService(WebServiceDelegate vdelegate) {
 		setDelegate(vdelegate);
 	}
-	
-	
+
 	/**
-	 * Get application setting from server 
+	 * Get application setting from server
+	 * 
 	 * @return JSONObject This object contains all keys/values of setting.
 	 * */
-	public void setGettingAppSetting()
-	{
+	public void setGettingAppSetting() {
 		url = SERVER_URL + "/api/Setting";
 	}
-	
+
 	/**
-	 * Get all news of specific date from server 
-	 * @param date The selected date.
+	 * Get all news of specific date from server
+	 * 
+	 * @param date
+	 *            The selected date.
 	 * @return JSONObject This object contains all keys/values of setting.
 	 * */
-	@SuppressLint("SimpleDateFormat") public void setGettingNewsData(Date date)
-	{
-		//Convert date to string with format yyyy-MM-dd
+	@SuppressLint("SimpleDateFormat")
+	public void setGettingNewsData(Date date) {
+		// Convert date to string with format yyyy-MM-dd
 		SimpleDateFormat fmtOut = new SimpleDateFormat("yyyy-MM-dd");
 		String dateStr = fmtOut.format(date);
 		url = SERVER_URL + "/api/news?selectedDate=" + dateStr;
-		
+
 	}
-	
+
 	/**
-	 * Get all events from server 
+	 * Get all events from server
+	 * 
 	 * @return JSONObject This object contains all keys/values of event.
 	 * */
-	public void setGettingEventsData()
-	{
+	public void setGettingEventsData() {
 		url = SERVER_URL + "/api/events";
 	}
-	
+
 	/**
 	 * Send a HTML request to server
-	 * @param url the HTML request string 
+	 * 
+	 * @param url
+	 *            the HTML request string
 	 * @return JSONObject This object contains all keys/values of response.
 	 * */
-	private void getDataFromUrl()
-	{
+	private void getDataFromUrl() {
 		Log.i("RoranLai", "WebService: getDataFromUrl " + url);
 		JSONObject responseString = null;
 		try {
 			DefaultHttpClient httpclient = new DefaultHttpClient();
-		    HttpResponse response;
+			HttpResponse response;
 			response = httpclient.execute(new HttpGet(url));
 			StatusLine statusLine = response.getStatusLine();
-			
-		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-		        ByteArrayOutputStream out = new ByteArrayOutputStream();
-		        response.getEntity().writeTo(out);
-		        out.close();
-		        Log.i("RoranLai", "WebService: response " + out.toString());
-		        responseString = new JSONObject(out.toString());
-		    } else{
-		        //Closes the connection.
-		        response.getEntity().getContent().close();
-		        throw new IOException(statusLine.getReasonPhrase());
-		    }
+
+			if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				response.getEntity().writeTo(out);
+				out.close();
+				Log.i("RoranLai", "WebService: response " + out.toString());
+				responseString = new JSONObject(out.toString());
+			} else {
+				// Closes the connection.
+				response.getEntity().getContent().close();
+				throw new IOException(statusLine.getReasonPhrase());
+			}
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -108,18 +108,15 @@ public class WebService extends AsyncTask <String, Void, JSONObject>
 
 	@Override
 	protected JSONObject doInBackground(String... arg0) {
-		if(url != null)
-		{
+		if (url != null) {
 			getDataFromUrl();
 		}
 		return null;
 	}
 
-
 	public WebServiceDelegate getDelegate() {
 		return delegate;
 	}
-
 
 	public void setDelegate(WebServiceDelegate delegate) {
 		this.delegate = delegate;
